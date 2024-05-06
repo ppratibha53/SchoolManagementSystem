22b9acc39c71301ad4a6ddf069856ac273cf9da5
Pratibha Pandey <ppratibhapandey53@gmail.com>
2024-05-06 15:17:13 +0530

    Add project files.

---
+++ StudentAdmissionManagement/Model/StudentAdmissionDetailsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentAdmissionManagement.Model
+{
+    public class StudentAdmissionDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string StudentClass   { get; set;}
+        public DateTime DateofJoining { get; set;}
+    
+    }
+}
---
+++ StudentAttendanceManagement/Model/StudentAttendanceDetailsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentAttendanceManagement.Model
+{
+    public class StudentAttendanceDetailsModel
+    { 
+        public int StudentID { get; set; }
+        public string StudentName { get; set;}
+        public double AttendencePercentage { get; set;}
+    }
+}
